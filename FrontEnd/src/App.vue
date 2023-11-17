9b2a37dc7366dd3cae58764b2608c1e65eb6da8c
Yemao Luo <leafysn@qq.com>
2023-11-17 16:19:05 +0800

    loop bug fix

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -33,11 +33,10 @@ import type {ExposeParam} from 'md-editor-v3';
 import {MdEditor} from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 import '@/App.css';
-import {getCursorPos, updateCursorPosition} from '@/utils/cursor';
 
 let theme = ref('light');
 let language = ref('en-US');
-let text = ref('')
+let text = ref('');
 let online = ref('0');
 const exclude = ref(['github', 'save']);
 const editorRef = ref<ExposeParam>();
@@ -47,7 +46,117 @@ let socketURL = 'ws://';
 let isConnected = ref(false);
 socketURL += window.location.host;
 socketURL += '/CollaborativeHandler';
-console.debug('socketURL:', socketURL);
+console.info('socketURL:', socketURL);
+
+let enableOnChange = true;
+
+function getCursorPositionInDivElement(divElement) {
+  const selection = window.getSelection();
+  const anchorNode = selection.anchorNode;
+  if (anchorNode && divElement.contains(anchorNode)) {
+    const textContent = divElement.textContent;
+    const anchorOffset = selection.anchorOffset;
+    let cursorIndex = 0;
+    for (let i = 0; i < textContent.length; i++) {
+      if (anchorNode === divElement && anchorOffset === i) {
+        break;
+      }
+
+      const node = divElement.childNodes[i];
+      const nodeTextLength = node.textContent.length;
+
+      if (anchorNode === node) {
+        cursorIndex += anchorOffset;
+        break;
+      }
+      cursorIndex += nodeTextLength;
+    }
+    return cursorIndex;
+  }
+  return -1;
+}
+
+function getCursorPos() {
+  const selection = window.getSelection();
+  let cursorElementText = '';
+  let position = 0;
+
+  if (selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    const cursorElement = range.startContainer.parentElement;
+    cursorElementText = cursorElement.innerText;
+    if (cursorElement.classList.contains('cm-line')) {
+      position = getCursorPositionInDivElement(cursorElement);
+    }
+  }
+  let lines = text.value.split('\n');
+  let count = 0;
+  lines.some((line) => {
+    if (line === cursorElementText) {
+      return true;
+    } else {
+      count += line.length + 1;
+    }
+    return false;
+  });
+
+  return count + position;
+}
+
+function updateCursorPosition(oldText, newText, cursorPosition) {
+  if (newText.length > oldText.length) {
+    let addPos = findAddedTextPositions(oldText, newText);
+    let start = addPos[0];
+    let end = addPos[1];
+    if (end < cursorPosition) {
+      return Math.min(cursorPosition + end - start, newText.length - 1);
+    } else {
+      return cursorPosition;
+    }
+  } else if (newText.length < oldText.length) {
+    let delPos = findDeletedTextPositions(oldText, newText);
+    let start = delPos[0];
+    let end = delPos[1];
+    console.info('start:', start, 'end:', end, 'cursorPosition:', cursorPosition);
+    if (start >= cursorPosition) {
+      return Math.min(cursorPosition, newText.length);
+    } else if (end < cursorPosition) {
+      return Math.max(cursorPosition - (end - start + 1), 0);
+    } else {
+      return Math.max(0, start);
+    }
+  } else {
+    return cursorPosition;
+  }
+}
+
+function findDeletedTextPositions(oldText, newText) {
+  let deletedPositions = [oldText.length];
+
+  for (let i = 0; i < newText.length; i++) {
+    if (oldText[i] !== newText[i]) {
+      deletedPositions[0] = i;
+      break;
+    }
+  }
+  deletedPositions[1] = deletedPositions[0] + oldText.length - newText.length - 1;
+
+  return deletedPositions;
+}
+
+function findAddedTextPositions(oldText, newText) {
+  let addedPositions = [];
+
+  for (let i = 0; i < oldText.length; i++) {
+    if (oldText[i] !== newText[i]) {
+      addedPositions[0] = i;
+      break;
+    }
+  }
+  addedPositions[1] = addedPositions[0] + newText.length - oldText.length;
+
+  return addedPositions;
+}
 
 function toggleTheme() {
   theme.value = theme.value === 'light' ? 'dark' : 'light';
@@ -73,8 +182,12 @@ const getStatusTooltip = computed(() => {
 });
 
 let onChange = (change) => {
-  console.debug('change:', change);
-  if (socket != null) {
+  if (!enableOnChange) {
+    return;
+  }
+
+  console.info('change:', change);
+  if (change.trim() !== '' && socket != null) {
     socket.send(change);
   }
 };
@@ -86,11 +199,13 @@ onMounted(() => {
 
   socket.addEventListener('message', (event) => {
     const message = JSON.parse(event.data);
-    console.debug('message:', message);
+    console.info('message:', message);
     if (message.type === 1) {
       online.value = message.message;
     } else if (message.type === 2) {
-      let preCursorPos = getCursorPos();
+      enableOnChange = false;
+
+      let preCursorPos = getCursorPos(text.value);
       let preText = text.value;
 
       text.value = message.message;
@@ -101,11 +216,13 @@ onMounted(() => {
 
       preCursorPos = updateCursorPosition(preText, message.message, preCursorPos);
       nextTick(() => {
-        console.debug('Configuring cursor position:', preCursorPos);
+        console.info('Configuring cursor position:', preCursorPos);
         const option = {
           cursorPos: preCursorPos,
         };
         editorRef.value?.focus(option);
+
+        enableOnChange = true;
       });
     }
   });
@@ -117,5 +234,5 @@ onMounted(() => {
   socket.addEventListener('close', () => {
     isConnected.value = false;
   });
-})
+});
 </script>
