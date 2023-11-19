6137cba95c32f832c6d996fc454aa74c0aa63e41
Yemao Luo <leafysn@qq.com>
2023-11-19 15:31:40 +0800

    Improve mobile and button experience

--- BackEnd/src/main/java/com/cpb/backend/handler/CollaborativeHandler.java
+++ BackEnd/src/main/java/com/cpb/backend/handler/CollaborativeHandler.java
@@ -3,70 +3,99 @@ package com.cpb.backend.handler;
 import com.cpb.backend.entity.Message;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-@ServerEndpoint("/CollaborativeHandler")
+@ServerEndpoint("/CollaborativeHandler/{id}")
 @Component
 @Slf4j
 public class CollaborativeHandler {
 
-    private static final CopyOnWriteArraySet<Session> sessions = new CopyOnWriteArraySet<>();
-    private static volatile String sharedText = "Hello Editor!";
+    private static final Map<String, List<Session>> sessions = new ConcurrentHashMap<>();
+    private static final Map<String, String> sharedTextMap = new ConcurrentHashMap<>();
     private static final ObjectMapper mapper = new ObjectMapper();
 
     @OnOpen
     @SneakyThrows
-    public void onOpen(Session session) {
-        sessions.add(session);
-        Message message = new Message(2, sharedText);
+    public void onOpen(Session session, @PathParam("id") String id) {
+        sessions.computeIfAbsent(id, k -> new ArrayList<>()).add(session);
+        Message message = new Message(2, sharedTextMap.getOrDefault(id, ""));
         session.getBasicRemote().sendText(mapper.writeValueAsString(message));
         log.info("New session opened: " + session.getId());
-        log.info("Number of sessions: " + sessions.size());
-        Message sessionCountMessage = new Message(1, String.valueOf(sessions.size()));
-        sendAllMessage(null, mapper.writeValueAsString(sessionCountMessage));
+        log.info("Number of sessions for ID {}: {}", id, sessions.get(id).size());
+        log.info("Total number of sessions: {}", getTotalNumberOfSessions());
+        Message sessionCountMessage = new Message(1, String.valueOf(getTotalNumberOfSessions()));
+        sendAllMessage(null, mapper.writeValueAsString(sessionCountMessage), id);
     }
 
     @OnClose
     @SneakyThrows
-    public void onClose(Session session) {
-        sessions.remove(session);
+    public void onClose(Session session, @PathParam("id") String id) {
+        List<Session> sessionList = sessions.get(id);
+        if (sessionList != null) {
+            sessionList.remove(session);
+            if (sessionList.isEmpty()) {
+                sessions.remove(id);
+            }
+        }
         session.close();
         log.info("Session closed: " + session.getId());
-        log.info("Number of sessions: " + sessions.size());
-        Message sessionCountMessage = new Message(1, String.valueOf(sessions.size()));
-        sendAllMessage(null, mapper.writeValueAsString(sessionCountMessage));
+        log.info("Number of sessions for ID {}: {}", id, sessions.get(id) == null ? 0 : sessions.get(id).size());
+        log.info("Total number of sessions: {}", getTotalNumberOfSessions());
+        Message sessionCountMessage = new Message(1, String.valueOf(getTotalNumberOfSessions()));
+        sendAllMessage(null, mapper.writeValueAsString(sessionCountMessage), id);
     }
 
     @OnMessage
     @SneakyThrows
-    public void onMessage(Session session, String receivedMessage) {
+    public void onMessage(Session session, String receivedMessage, @PathParam("id") String id) {
         log.info("Message received: " + receivedMessage);
-        sharedText = receivedMessage;
-        Message message = new Message(2, sharedText);
-        sendAllMessage(session, mapper.writeValueAsString(message));
+        sharedTextMap.put(id, receivedMessage);
+        Message message = new Message(2, sharedTextMap.getOrDefault(id, ""));
+        sendAllMessage(session, mapper.writeValueAsString(message), id);
     }
 
     @OnError
     @SneakyThrows
-    public void onError(Session session, Throwable throwable) {
+    public void onError(Session session, Throwable throwable, @PathParam("id") String id) {
         log.error("Error occurred: " + throwable.getMessage());
-        sessions.remove(session);
+        List<Session> sessionList = sessions.get(id);
+        if (sessionList != null) {
+            sessionList.remove(session);
+            if (sessionList.isEmpty()) {
+                sessions.remove(id);
+            }
+        }
         session.close();
     }
 
     @SneakyThrows
-    public static void sendAllMessage(Session session, String message) {
-        log.info("Send Message: " + message);
-        for (Session webSocket : sessions) {
-            if (session != null && session.getId().equals(webSocket.getId())) {
-                continue;
+    public static void sendAllMessage(Session session, String message, String id) {
+        log.info("Sending message with length {} to {} sessions", message.length(), sessions.get(id) == null ? 0 : sessions.get(id).size());
+        List<Session> sessionList = sessions.get(id);
+        if (sessionList != null) {
+            for (Session webSocket : sessionList) {
+                if (session != null && session.getId().equals(webSocket.getId())) {
+                    continue;
+                }
+                webSocket.getBasicRemote().sendText(message);
             }
-            webSocket.getBasicRemote().sendText(message);
         }
+    }
+
+    private static int getTotalNumberOfSessions() {
+        int total = 0;
+        for (List<Session> sessionList : sessions.values()) {
+            total += sessionList.size();
+        }
+        return total;
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 ### Version
 1. **V1.0**: Implemented basic functions that allow users to edit a real-time update md file
 2. **V1.1**: Fix bugs; Improve mobile and button experience
+3. **V2.0**: Support multi documents.
